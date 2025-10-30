96a84d709f6b657acd765b91cdba07f28e1c624c
CarloMart88 <carlo.martino1988@gmail.com>
2025-10-30 12:14:21 +0100

    second snack

--- snack.js
+++ snack.js
@@ -1,6 +1,6 @@
 /*Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?*/
-
+/*
 
 const hamburger = { name: "Cheese Burger", weight: 250 };
 const secondBurger = hamburger;
@@ -18,3 +18,20 @@ Double Cheese Burger
 */ 
 
 
+
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = {...hamburger};
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad
+
+
+/*P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?  3 oggetti  */
